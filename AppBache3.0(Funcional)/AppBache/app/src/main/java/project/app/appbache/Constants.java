c03ad139124536aa3273e24969a08d887fbef014
David-A-Galvez <55710686+David-A-Galvez@users.noreply.github.com>
2019-12-06 05:08:31 -0700

    Update Constants.java

--- AppBache3.0(Funcional)/AppBache/app/src/main/java/project/app/appbache/Constants.java
+++ AppBache3.0(Funcional)/AppBache/app/src/main/java/project/app/appbache/Constants.java
@@ -2,7 +2,7 @@ package project.app.appbache;
 
 public class Constants {
     public static final String PROTOCOL = "http://";
-    public static final String ROOT_URL = PROTOCOL + " 177.228.77.201";
+    public static final String ROOT_URL = PROTOCOL + " ";
     public static final String URL_REGISTRO = ROOT_URL+"/Registro.php";
     public static final String URL_LOGIN = ROOT_URL+"/Login.php";
 
